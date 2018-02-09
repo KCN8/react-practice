756f045b02a43000541b7c1efe2d39d501dd89ca
Casey Herold <caseynate@gmail.com>
2018-02-09 08:47:10 -0700

    got through basic features and working with lists and conditionals. moving on to component and element styling in react🥊

--- src/App.js
+++ src/App.js
@@ -5,33 +5,72 @@ import Person from './Person/Person'
 class App extends Component {
 
   state = {
-    people: [
-      { name: "Casey", age: 32 },
-      { name: "Matt", age: 31 },
-      { name: "CJ", age: 30 }
-    ]
+    persons: [
+      { id: 1, name: "KC", age: 33 },
+      { id: 2, name: "Matt", age: 32 },
+      { id: 3, name: "Kyle", age: 30 }
+    ],
+    showPersons: false
   }
 
-  switchNameHandler = () => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+
+    persons[personIndex] = person
+
+    this.setState({ persons: persons })
+  }
+
+  deletePersonHandler = (personIndex) =>{
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = (newName) => {
+    const doesShow = this.state.showPersons
     this.setState({
-      people: [
-        { name: "KC", age: 32 },
-        { name: "Matt", age: 31 },
-        { name: "CJ", age: 0 }
-      ] })
+      showPersons: !doesShow
+    })
   }
 
   render() {
+
+    let person = null
+
+    if (this.state.showPersons){
+      person = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>REACT APP STUFF</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>Hobbies: Yoga</Person> 
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
+        <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {person} 
       </div>
-    );
+    )
   }
 }
 
